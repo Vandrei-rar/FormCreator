b53614f014d6ada2417cd204c14d83a459922ca7
Vandrei Ribeiro <vandreiribeiro@gmail.com>
2023-08-14 11:58:28 -0300

    First commit

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { Button, Text, Container, Flex } from "@chakra-ui/react";
-import NextLink from "next/link";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Flex height="100vh" justifyContent={"center"} alignItems={"center"}>
       <Container maxW={"container.xl"} centerContent>
         <Text fontSize={"4xl"}>Crie seu formulário</Text>
-        <NextLink href='/form'>
-          <Button colorScheme="orange" rightIcon={<ArrowForwardIcon />}>
-            Começar
-          </Button>
-        </NextLink>
+        <Button colorScheme="orange" rightIcon={<ArrowForwardIcon />}>
+          Voltar
+        </Button>
       </Container>
     </Flex>
   );
