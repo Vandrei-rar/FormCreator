5e6a5b4a58eda93b05276a14e82e08393b85dbe3
Vandrei Ribeiro <vandreiribeiro@gmail.com>
2023-08-29 16:56:09 -0300

    checkpoint

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,25 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+import { log } from "console";
+const prisma = new PrismaClient()
+
+export async function POST(req: Request) {
+    const data = await req.formData();
+    
+    return NextResponse.json({data: data})
+
+    // const form = await prisma.form.create({
+    //     data: {
+    //         name: 'Formulario teste',
+    //         description: 'Descrição teste',
+    //     }
+    // })
+
+    //return NextResponse.json({form});
+}
 
 export async function GET() {
-    return NextResponse.json({data: 'Olá mundo'});
+    const data = await prisma.form.findMany();
+
+    return NextResponse.json({data})
 }
